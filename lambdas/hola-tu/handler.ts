cd6a3f93d0153bc4057d378f5b848aabe1f692b7
AV-Alumno <AV-Alumno@duoc.acad>
2026-03-17 21:07:21 -0300

    ArregloParaDeploy

--- lambdas/hola-tu/handler.ts
+++ lambdas/hola-tu/handler.ts
@@ -1,16 +1,22 @@
 import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 
+
 const client = new DynamoDBClient({});
 
 export const handler = async (event: any) => {
   const tableName = process.env.TABLE_NAME!;
 
   const result = await client.send(new ScanCommand({ TableName: tableName }));
-  const items = result.Items ? result.Items.map(item => unmarshall(item)) : [];
+
+  // Aquí es donde agregamos el ": any" para resolver el error TS7006
+  const items = result.Items 
+    ? result.Items.map((item: any) => unmarshall(item)) 
+    : [];
 
   return {
     statusCode: 200,
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       message: 'Hola Mundo! 🚀',
       items,
